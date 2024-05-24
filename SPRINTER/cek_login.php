7706ee662387ec4f87af13025f5a911650c587ef
Achild29 <Wahyu.Khoirurrizal@gmail.com>
2024-05-24 20:52:10 +0700

    test comment di sistem_info

--- SPRINTER/cek_login.php
+++ SPRINTER/cek_login.php
@@ -6,17 +6,18 @@ $user=$_POST['nama'];
 $sqlUser=mysqli_query($connect,"select * from admin where username='$user' and password='$pass'");
 $countUser=mysqli_num_rows($sqlUser);
 $rsUser=mysqli_fetch_array($sqlUser);
-/*
-$sqlWarga=mysqli_query($connect,"select * from warga_pabuaran where NIK='$user' and Password='$pass'");
-$countWarga=mysqli_num_rows($sqlWarga);
-$rsWarga=mysqli_fetch_array($sqlWarga);
-*/
+
+$sqlProdi=mysqli_query($connect,"select * from prodi where kode_prodi='$user' and password='$pass'");
+$countProdi=mysqli_num_rows($sqlProdi);
+$rsProdi=mysqli_fetch_array($sqlProdi);
+
 if (empty($user) || empty($pass)){
 	echo "<script language='javascript'> alert('Username dan Password tidak boleh kosong!');
 	window.location = 'javascript:history.go(-1)'</script>";
-}else if ($countUser == 0 && $countWarga == 0){
-	echo "<script language='javascript'> alert('Username dan password salah!');
-	window.location = 'javascript:history.go(-1)'</script>";
+}else if ($countUser == 0 && $countProdi == 0){
+	echo "$countProdi";
+	/*echo "<script language='javascript'> alert('Username dan password salah!');
+	window.location = 'javascript:history.go(-1)'</script>";*/
 }else{
 	if ($countUser > 0){
 		if(!isset($_SESSION)){
@@ -27,15 +28,15 @@ if (empty($user) || empty($pass)){
 			$_SESSION['level']='Admin';
 	
 			header('location: index.php');
-	} /*else if ($countWarga > 0){
+	} else if ($countProdi > 0){
 		if(!isset($_SESSION)){
 			session_start();
 		}
-			$_SESSION['id']=$rsWarga['NIK'];
-			$_SESSION['nama']=$rsWarga['Nama'];
-			$_SESSION['level']='Warga';
+			$_SESSION['id']=$rsProdi['id'];
+			$_SESSION['nama']=$rsProdi['username'];
+			$_SESSION['level']='Prodi';
 	
-			header('location: updateMaster.php');
-	}*/
+			header('location: mkp.php');
+	}
 }
 ?>
