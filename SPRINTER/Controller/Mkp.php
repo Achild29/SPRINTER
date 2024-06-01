66884966feea3cb17c4e4ca307b6fddb7c305a4e
LEAK-Jnr <leovansandie@gmail.com>
2024-06-01 13:00:36 +0700

    mkp insertion done, please re-check

--- SPRINTER/Controller/Mkp.php
+++ SPRINTER/Controller/Mkp.php
@@ -5,11 +5,15 @@ include '../koneksi.php';
 $kode_prodi = $_POST['kode_prodi'];
 $nama_mkp = $_POST['nama_mkp'];
 $sks = $_POST['sks'];
-$kode_mkp = ""+$kode_prodi+$nama_mkp+$sks;
+$mk = explode(' ',$nama_mkp);
+if(count($mk)>1){
+    $kode_mkp = "".$kode_prodi.substr($mk[0],0,3).substr($mk[count($mk)-1],0,2).$sks;
+}
+else $kode_mkp = "".substr($kode_prodi,5).$nama_mkp.$sks;
 
 
 
 $sql = "INSERT INTO mkp (kode_mkp, kode_prodi, nama_mkp, sks) VALUES ('$kode_mkp','$kode_prodi','$nama_mkp','$sks')";
 $simpan = mysqli_query($connect,$sql) or die ("Gagal Tambah : ".mysqli_error($connect));
-header("location: ../index.php")
+header("location: ../mkp.php")
 ?>
