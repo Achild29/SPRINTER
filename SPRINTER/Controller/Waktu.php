2d71b46b8194d4b45eb5e4f6f27273a96be4cace
LEAK-Jnr <leovansandie@gmail.com>
2024-06-01 13:01:39 +0700

    form adjustment, time input format.

--- SPRINTER/Controller/Waktu.php
+++ SPRINTER/Controller/Waktu.php
@@ -1,16 +1,16 @@
 <?php
 
-include 'koneksi.php';
+include '../koneksi.php';
 
-$kode_waktu = $_POST['kode_waktu'];
-$reguler = $_POST['reguler'];
+$reguler = $_POST['reg'];
 $hari = $_POST['hari'];
-$jam_mulai = $_POST['jam_mulai'];
-$jam_selesai = $_POST['jam_selesai'];
+$jam_mulai = $_POST['jam'];
+$jam_selesai = $_POST['jam'];
+$kode_waktu = substr($hari,0,3).$reguler.$jam_mulai;
 
 
 
 $sql = "INSERT INTO waktu (kode_waktu, reguler, hari, jam_mulai, jam_selesai) VALUES ('$kode_waktu', '$reguler', '$hari', '$jam_mulai', '$jam_selesai')";
 $simpan = mysqli_query($connect,$sql) or die ("Gagal Tambah : ".mysqli_error($connect));
-header("location: index.php")
+header("location: ../waktu.php")
 ?>
