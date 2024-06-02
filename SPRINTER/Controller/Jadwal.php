c06adeed9532e9e8f31d9d1d56c28e361fa7635e
LEAK-Jnr <leovansandie@gmail.com>
2024-06-02 11:08:53 +0700

    minor bug fix: fix kode_jadwal generation to avoid duplicate. important note: please change your database (sql) structure of table 'jadwal' change the length of kode_jadwal to be 20.

--- SPRINTER/Controller/Jadwal.php
+++ SPRINTER/Controller/Jadwal.php
@@ -3,10 +3,11 @@
 include '../koneksi.php';
 
 $kode_waktu = $_POST['kode_waktu'];
+$kode_prodi = $_POST['kode_prodi'];
 $kode_mkp=$_POST['kode_mkp'];
 $pekan = $_POST['pekan'];
 $kelas = $_POST['kelas'];
-$kode_jadwal = substr($kode_waktu,0,3).substr($kode_mkp,0,3).$pekan;
+$kode_jadwal = substr($kode_waktu,0,6).substr($kode_prodi,3,2).substr($kode_mkp,5).substr($kelas,7,2).$pekan;
 
 
 $sql = "INSERT INTO jadwal (kode_jadwal, kode_waktu, kode_mkp, kelas, pekan) VALUES ('$kode_jadwal','$kode_waktu', '$kode_mkp', '$kelas', '$pekan')";
