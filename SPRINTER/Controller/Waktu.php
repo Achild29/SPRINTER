9c6994d8ed1f27cf1914b3800506c717af61a47a
LEAK-Jnr <leovansandie@gmail.com>
2024-06-09 11:29:30 +0700

    minor bug fix: 'jam_selesai' 24-hour format

--- SPRINTER/Controller/Waktu.php
+++ SPRINTER/Controller/Waktu.php
@@ -6,7 +6,7 @@ $reguler = $_POST['reg'];
 $hari = $_POST['hari'];
 $jam_mulai = $_POST['jam'];
 $jam_selesai_str= strtotime("+100 minutes",strtotime($jam_mulai));
-$jam_selesai = date ('h:i:s', $jam_selesai_str);
+$jam_selesai = date ('H:i:s', $jam_selesai_str);
 $kode_waktu = substr($hari,0,3).$reguler.$jam_mulai;
 
 
