d76d481b681b4d898d4e68303a9cb3400b6d63a5
Rully31 <rully.inkiriwang@gmail.com>
2024-08-19 17:04:39 +0700

    UPDATE NOTIFIKASI

--- SPRINTER/admin-account-settings.php
+++ SPRINTER/admin-account-settings.php
@@ -98,41 +98,27 @@ if (empty($_SESSION['id']) and empty($_SESSION['nama']) and empty($_SESSION['lev
 
             <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
               <i class="bi bi-chat-left-text"></i>
-              <span class="badge bg-success badge-number">2</span>
+                 <span class="badge bg-success badge-number">0</span>
             </a>
 
             <!-- Messages Dropdown Items -->
             <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
               <li class="dropdown-header">
-                You have new messages
-                <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                Tidak Memiliki Pesan Baru
+                <!-- 
+<a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+ -->
               </li>
               <li>
                 <hr class="dropdown-divider">
               </li>
 
-              <li class="message-item">
-                <a href="#">
-                  <div>
-                    <h4>Pengajuan Anda di ACC</h4>
-                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                    <p>4 hrs. ago</p>
-                  </div>
-                </a>
-              </li>
+                
               <li>
                 <hr class="dropdown-divider">
               </li>
 
-              <li class="message-item">
-                <a href="#">
-                  <div>
-                    <h4>Pengajuan Anda di REJECT</h4>
-                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                    <p>6 hrs. ago</p>
-                  </div>
-                </a>
-              </li>
+                 
               <li>
                 <hr class="dropdown-divider">
               </li>
